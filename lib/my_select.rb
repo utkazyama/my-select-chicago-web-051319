c0daf46eb78746e21fe17d31a0c165e2e4e25a0b
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-17 02:00:36 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i=0
+ selected=[]
+ 
+  while i<collection.length
+   if yield collection[i]
+   selected << collection[i]
+   end
+  i += 1
+  end
+ selected
 end
